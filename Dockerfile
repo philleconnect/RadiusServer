35c1a6b7026237f140e00987c12411177d7ac8d2
polarwinkel <it@polarwinkel.de>
2018-06-10 22:05:11 +0200

    including env-vars, cleanup for beta

--- Dockerfile
+++ Dockerfile
@@ -10,11 +10,10 @@ USER root
 
 RUN apt-get update && apt-get install -y --no-install-recommends freeradius freeradius-ldap freeradius-utils nano
 
-COPY clients.conf /etc/freeradius/3.0/clients.conf.2
-RUN cat /etc/freeradius/3.0/clients.conf.2 >> /etc/freeradius/3.0/clients.conf
-COPY users /etc/freeradius/3.0/users.2
-RUN cat /etc/freeradius/3.0/users.2 >> /etc/freeradius/3.0/users
+COPY clients.conf /root/clients.conf
+COPY users /root/users
 COPY ldapmod.conf /root/ldap
+
 COPY entrypoint.sh /
 
 CMD "./entrypoint.sh"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # check if environment variables are set with -e option:
+if [[ -z "$RADIUS_LISTEN_IP_RANGE" ]]; then
+    echo -n >&2 "Error: Container not configured and RADIUS_LISTEN_IP_RANGE not set. "
+    echo >&2 "Did you forget to add -e RADIUS_LISTEN_IP_RANGE=... ?"
+    exit 1
+fi
+if [[ -z "$RADIUS_PASSWORD" ]]; then
+    echo -n >&2 "Error: Container not configured and RADIUS_PASSWORD not set. "
+    echo >&2 "Did you forget to add -e RADIUS_PASSWORD=... ?"
+    exit 1
+fi
 if [[ -z "$SLAPD_PASSWORD" ]]; then
     echo -n >&2 "Error: Container not configured and SLAPD_PASSWORD not set. "
     echo >&2 "Did you forget to add -e SLAPD_PASSWORD=... ?"
@@ -26,9 +36,14 @@ fi
 sed -i "s|SLAPD_PASSWORD|$SLAPD_PASSWORD|g" /root/ldap
 sed -i "s|SLAPD_DOMAIN0|$SLAPD_DOMAIN0|g" /root/ldap
 sed -i "s|SLAPD_DOMAIN1|$SLAPD_DOMAIN1|g" /root/ldap
-
 cp /root/ldap /etc/freeradius/3.0/mods-enabled/ldap
 
-service freeradius start
+sed -i "s|RADIUS_LISTEN_IP_RANGE|$RADIUS_LISTEN_IP_RANGE|g" /root/clients.conf
+sed -i "s|RADIUS_PASSWORD|$RADIUS_PASSWORD|g" /root/clients.conf
+cat /root/clients.conf >> /etc/freeradius/3.0/clients.conf
+cat /root/users >> /etc/freeradius/3.0/users
 
-while true; do sleep 1; done # keep container running for debugging...
+#service freeradius start
+freeradius -f
+
+#while true; do sleep 1; done # keep container running for debugging...
