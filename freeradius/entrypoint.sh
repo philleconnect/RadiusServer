cdc4c8741cb3ac8d0fcb73ad61826073051f3c7c
Johannes Kreutz <jkm@jkm-marburg.de>
2020-08-12 10:56:59 +0200

    Move to v2 plugin structure

--- freeradius/entrypoint.sh
+++ freeradius/entrypoint.sh
@@ -17,9 +17,9 @@ if [[ -z "$SLAPD_PASSWORD" ]]; then
     exit 1
 fi
 if [[ -z "$SLAPD_DOMAIN0" ]]; then
-        echo -n"SLAPD_DOMAIN0 not set."
-        echo -n"I am using 'local'"
-        SLAPD_DOMAIN0='local'
+    echo -n"SLAPD_DOMAIN0 not set."
+    echo -n"I am using 'local'"
+    SLAPD_DOMAIN0='local'
 fi
 if [[ -z "$SLAPD_DOMAIN1" ]]; then
     echo -n"SLAPD_DOMAIN1 not set."
@@ -36,30 +36,14 @@ cp -r /root/ldap /etc/freeradius/3.0/mods-enabled/ldap
 sed -i "s|RADIUS_LISTEN_IP_RANGE|$RADIUS_LISTEN_IP_RANGE|g" /etc/freeradius/3.0/clients.conf
 sed -i "s|RADIUS_PASSWORD|$RADIUS_PASSWORD|g" /etc/freeradius/3.0/clients.conf
 
-if ( $RADIUS_REQUIRE_TEACHER ); then
-    #if [ -f /root/users.bak ]; then
-    #    cp /root/users.bak /etc/freeradius/3.0/users
-    #else
-    #    cp /etc/freeradius/3.0/users /root/users.bak
-    #fi
-    #cat /root/users >> /etc/freeradius/3.0/users
-    if [ ! -f /root/default.bak ]; then
-        cp /etc/freeradius/3.0/sites-enabled/default /root/default.bak
-    fi
-    cp /root/default /etc/freeradius/3.0/sites-enabled/default
-    echo "I configured radius to authenticate only teachers"
-else
-    #if [ -f /root/users.bak ]; then
-    #    cp /root/users.bak /etc/freeradius/3.0/users
-    #fi
-    if [ -f /root/default.bak ]; then
-        cp /root/default.bak /etc/freeradius/3.0/sites-enabled/default
-    fi
-    echo "I configured radius to authenticate any user"
+
+if [ ! -f /root/default.bak ]; then
+    cp /etc/freeradius/3.0/sites-enabled/default /root/default.bak
 fi
+cp /root/default /etc/freeradius/3.0/sites-enabled/default
+echo "I configured radius to authenticate only users in the wifi group"
 
 
 freeradius -f
-#radiusd -f
 #service freeradius start
 #while true; do sleep 1; done # keep container running for debugging...
